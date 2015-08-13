5058e9e7c79450c5e97c8e0c9c9e24b02b28ce69
Gal Margalit <gal.margalit@alcatel-lucent.com>
2015-08-13 10:59:59 +0000

    mistral-dashboard Execution enhancements
    * fixed main table:
      a. execution id now refers to overview screen
      b. task refer to corresponding task
    * added execution overview screen
    * removed duplicate string from prettyprint, shortage should be done on client side, (we don't use it atm)
    
    * Screenshots:
    http://i57.tinypic.com/2817r5c.png
    http://i58.tinypic.com/23uydmo.png
    
    Partially implements blueprint: mistral-dashboard-executions-screen
    
    Change-Id: I4c580e21299aaccf4cf012c88d2d4c0d91de5710

--- mistraldashboard/executions/views.py
+++ mistraldashboard/executions/views.py
@@ -14,9 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from django.views import generic
+
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import exceptions
 from horizon import tables
 
 from mistraldashboard import api
+from mistraldashboard.default.utils import prettyprint
 from mistraldashboard.executions.tables import ExecutionsTable
 from mistraldashboard.executions.tables import TaskTable
 
@@ -35,3 +42,28 @@ class TaskView(tables.DataTableView):
 
     def get_data(self):
         return api.task_list(self.request, self.kwargs['execution_id'])
+
+
+class DetailView(generic.TemplateView):
+    template_name = 'mistral/executions/detail.html'
+    page_title = _("Execution Overview")
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        execution = self.get_data(self.request, **kwargs)
+        execution.input = prettyprint(execution.input)
+        execution.output = prettyprint(execution.output)
+        execution.params = prettyprint(execution.params)
+        context['execution'] = execution
+        return context
+
+    def get_data(self, request, **kwargs):
+        try:
+            execution_id = kwargs['execution_id']
+            execution = api.execution_get(request, execution_id)
+        except Exception:
+            msg = _('Unable to get execution "%s".') % execution_id
+            redirect = reverse('horizon:mistral:executions:index')
+            exceptions.handle(self.request, msg, redirect=redirect)
+
+        return execution
