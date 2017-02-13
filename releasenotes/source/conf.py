c1bb2c7c757ffb7415e436a704998a3c1c665fa7
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2017-02-13 11:15:42 +0800

    Fix 'tox -ereleasenotes' Error
    1. replace reno.sphinxtest with reno.sphinxext for Sphinx extensions as
    sphinxtest is not supported
    2. remove 'Indices and tables' from index.rst as it's not found.
    
    Change-Id: Idb9e75a326e5b1fc39a28aebb6eb58182e4a27c5

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'oslosphinx',
-    'reno.sphinxtest',
+    'reno.sphinxext',
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
